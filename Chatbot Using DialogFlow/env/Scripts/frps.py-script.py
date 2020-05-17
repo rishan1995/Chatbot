1d220215f3e998e8d9325a2404ffa967191e5620
rishan1995 <rishan1995@gmail.com>
2020-05-17 16:06:45 +0530

    initial commit

---
+++ Chatbot Using DialogFlow/env/Scripts/frps.py-script.py
@@ -0,0 +1,12 @@
+#!c:\users\rishan\movie_bot\env\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'ngrok==0.0.1','console_scripts','frps.py'
+__requires__ = 'ngrok==0.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('ngrok==0.0.1', 'console_scripts', 'frps.py')()
+    )
